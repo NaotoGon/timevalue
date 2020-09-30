16e1c566f67dd627fb14918ca642fac8d2598d42
NaotoGon <naotogon5710@gmail.com>
2020-10-01 04:20:27 +0900

    ActiveHash

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,6 +1,7 @@
 class Mentor < ApplicationRecord
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'メンターとして登録' }
+    { id: 2, name: 'メンターとして登録しない' }
+    { id: 3, name: 'メンターとして登録する' }
   ]
 end
