24287072a4f00d8749d906915a0dfd8357bd63b1
NaotoGon <naotogon5710@gmail.com>
2020-10-01 04:01:55 +0900

    ActiveHash

---
+++ app/models/category.rb
@@ -0,0 +1,10 @@
+class Category < ApplicationRecord
+  self.data = [
+   { id: 1, name: '--' },
+   { id: 2, name: '語学' },
+   { id: 3, name: 'プログラミング' },
+   { id: 4, name: '科学' },
+   { id: 5, name: '相談' },
+   { id: 6, name: 'その他' }
+ ]
+end
---
+++ app/models/mentor.rb
@@ -0,0 +1,6 @@
+class Mentor < ApplicationRecord
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'メンターとして登録' }
+  ]
+end
