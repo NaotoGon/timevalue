915c70330a30ee1900d55e12d928321835ba023c
NaotoGon <naotogon5710@gmail.com>
2020-10-01 16:04:38 +0900

    修正

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,7 +1,7 @@
-class Mentor < ApplicationRecord
+class Mentor < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'メンターとして登録しない' }
+    { id: 2, name: 'メンターとして登録しない' },
     { id: 3, name: 'メンターとして登録する' }
   ]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :mentor
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :mentor
 end
