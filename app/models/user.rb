55e45095649b37e601b27950c3814f05a681a2f8
NaotoGon <naotogon5710@gmail.com>
2020-10-01 17:32:34 +0900

    プロフィール画像

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :mentor
-  
+  has_one_attached :image
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
