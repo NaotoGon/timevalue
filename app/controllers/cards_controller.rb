5b03d1bafbd917e761e2a8a9d6dbde4bb7103d50
NaotoGon <naotogon5710@gmail.com>
2020-10-02 00:27:58 +0900

    カード表示

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,10 +8,17 @@ class CardsController < ApplicationController
     description: 'test',
     card: params[:card_token]
     )
+
+    card = Card.new(
+      card_token: params[:card_token],
+      customer_token: customer.id,
+      user_id: current_user.id
+    )
+
     if card.save
       redirect_to root_path
     else
-      redirect_to "new" # カード登録画面
+      redirect_to "new"
     end
   end
 end
