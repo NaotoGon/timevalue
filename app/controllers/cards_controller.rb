e8d8c22e5b2a4cc20638c4b4496d7e6d5acaf2fc
NaotoGon <naotogon5710@gmail.com>
2020-10-01 19:38:50 +0900

    コントローラ

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,17 @@
 class CardsController < ApplicationController
   def new
   end
+
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create(
+    description: 'test',
+    card: params[:card_token]
+    )
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to "new" # カード登録画面
+    end
+  end
 end
